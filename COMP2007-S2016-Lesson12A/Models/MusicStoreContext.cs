f111bff72c993a74875e6f9873948e92c89e5a70
macravenduarte <duarte.marco242@gmail.com>
2016-08-04 09:28:58 -0400

    Add project files.

---
+++ COMP2007-S2016-Lesson12A/Models/MusicStoreContext.cs
@@ -0,0 +1,20 @@
+namespace COMP2007_S2016_Lesson12A.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class MusicStoreContext : DbContext
+    {
+        public MusicStoreContext()
+            : base("name=MusicStoreConnection")
+        {
+        }
+
+        public virtual DbSet<Album> Albums { get; set; }
+        public virtual DbSet<Genre> Genres { get; set; }
+        public virtual DbSet<Artist> Artists { get; set; }
+
+    }
+}
